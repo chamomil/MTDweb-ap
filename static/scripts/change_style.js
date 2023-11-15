b6cf9aeb823b6f54e850fef84929898aac32f1f7
alyona <alyonka.m.by@gmail.com>
2023-11-16 01:10:02 +0300

    add ability to change color of text and bg

--- static/scripts/change_style.js
+++ static/scripts/change_style.js
@@ -12,13 +12,25 @@ function handleCheckboxChange() {
     if (checkbox.checked) {
         change_container.style.backgroundColor = 'white';
         let div = document.createElement('div');
+        let div2 = document.createElement('div');
+        let div3 = document.createElement('div');
         let sizeText = document.createElement('h4');
+        let colorText = document.createElement('h4');
+        let colorBgText = document.createElement('h4');
         let sizePlus = document.createElement('button');
         let sizeMinus = document.createElement('button');
+        let colorInput = document.createElement('input');
+        let colorBgInput = document.createElement('input');
 
         sizeText.innerHTML = 'font-size';
+        colorText.innerHTML = 'color';
+        colorBgText.innerHTML = 'background-color';
         sizePlus.innerHTML = '+';
         sizeMinus.innerHTML = '-';
+        colorInput.type = "color";
+        colorBgInput.type = "color";
+        colorInput.value = "#B846E0"
+        colorBgInput.value = "#62BCF5"
 
         sizePlus.addEventListener('click', function () {
             let currentFontSize = parseInt(window.getComputedStyle(document.body).fontSize);
@@ -32,9 +44,23 @@ function handleCheckboxChange() {
             document.body.style.fontSize = (currentFontSize - 2) + 'px';
         });
 
+        colorInput.addEventListener('change', function () {
+            document.body.style.color = colorInput.value;
+        });
+
+        colorBgInput.addEventListener('change', function () {
+            document.body.style.backgroundColor = colorBgInput.value;
+        });
+
         div.appendChild(sizeText);
         div.appendChild(sizePlus);
         div.appendChild(sizeMinus);
+        div2.appendChild(colorText);
+        div2.appendChild(colorInput);
+        div3.appendChild(colorBgText);
+        div3.appendChild(colorBgInput);
         change_container.appendChild(div);
+        change_container.appendChild(div2);
+        change_container.appendChild(div3);
     }
 }
